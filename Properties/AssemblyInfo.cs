20218a2f7d536c1eb3dcf536dbce7a23a9a910b9
smandyscom <smandyscom@gmail.com>
2018-07-02 00:12:04 +0800

    initialize

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FittingTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FittingTester")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a43dee3e-8e3d-43f8-89d6-5ef7dcb8a69d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenCvSharp;
+using WindowsFormsApp2;
+using WindowsFormsApp2.Fitting;
+
+namespace FittingTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestEllipseCoeficients()
+        {
+            Mat __coords = Mat.Ones(1, 2, MatType.CV_64FC1);
+            __coords.Set<double>(0, 0, 2);
+            __coords.Set<double>(0, 1, 3);
+
+            Mat __output = Fitting.GenerateEllipseCoeff(__coords);
+        }
+
+        [TestMethod]
+        public void TestPolynominalCoefficients()
+        {
+            Mat __coords = Mat.Ones(1, 2, MatType.CV_64FC1);
+            __coords.Set<double>(0, 0, 2);
+            __coords.Set<double>(0, 1, 3);
+
+            Mat __output = Fitting.GeneratePolynomialCoeff(__coords,1);
+
+            __output = Fitting.GeneratePolynomialCoeff(__coords, 2);
+            __output = Fitting.GeneratePolynomialCoeff(__coords, 3);
+        }
+
+        [TestMethod]
+        public void TestDataFitting1()
+        {
+            Mat __xVector = Mat.Ones(5, 2, MatType.CV_64FC1);
+            Mat __yVector = Mat.Zeros(5, 1, MatType.CV_64FC1);
+
+            Cv2.Randn(__xVector, 1, 1);
+
+            Mat __output = Fitting.DataFitting(__xVector, __yVector, Fitting.FittingCategrory.Polynominal, 1);
+        }
+    }
+}
