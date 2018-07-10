fd8ca0ffa4615be05bb2a4f004d372438abcda41
smandyscom <smandyscom@gmail.com>
2018-07-10 22:43:13 +0800

    Hierarchy route find tested

--- UnitTest2.cs
+++ UnitTest2.cs
@@ -20,6 +20,31 @@ namespace FittingTester
     [TestClass]
     public class UnitTest2
     {
+        HierarchyTreeNode<Data> __root00;
+        HierarchyTreeNode<Data> __branch10 ;
+        HierarchyTreeNode<Data> __branch11 ;
+        HierarchyTreeNode<Data> __branch20;
+        HierarchyTreeNode<Data> __leaf21 ;
+        HierarchyTreeNode<Data> __leaf22 ;
+        HierarchyTreeNode<Data> __leaf23 ;
+        HierarchyTreeNode<Data> __leaf24 ;
+        HierarchyTreeNode<Data> __leaf30 ;
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+           __root00 = new HierarchyTreeNode<Data>(new Data() { generation = 0, order = 0 }, null);
+           __branch10 = new HierarchyTreeNode<Data>(new Data() { generation = 1, order = 0 }, __root00);
+           __branch11 = new HierarchyTreeNode<Data>(new Data() { generation = 1, order = 1 }, __root00);
+           __branch20 = new HierarchyTreeNode<Data>(new Data() { generation = 2, order = 0 }, __branch10);
+           __leaf21 = new HierarchyTreeNode<Data>(new Data() { generation = 2, order = 1 }, __branch10);
+           __leaf22 = new HierarchyTreeNode<Data>(new Data() { generation = 2, order = 2 }, __branch10);
+           __leaf23 = new HierarchyTreeNode<Data>(new Data() { generation = 2, order = 3 }, __branch11);
+           __leaf24 = new HierarchyTreeNode<Data>(new Data() { generation = 2, order = 4 }, __branch11);
+           __leaf30 = new HierarchyTreeNode<Data>(new Data() { generation = 3, order = 0 }, __branch20);
+        }
+
         [TestMethod]
         public void TestMethodHierarchyNode1()
         {
@@ -28,11 +53,12 @@ namespace FittingTester
             // |-------Branch10------Branch20-------------------Leaf30
             // |                 |---Leaf21
             // |                 |---Leaf22
-            // L-------Branch20------Leaf23
+            // L-------Branch11------Leaf23
             //                   |--Leaf24
 
-            Data __root00 = new Data(){ generation=0, order=0 };
+           var __list = HierarchyTreeNode<Data>.Path(__leaf30, __leaf24);
 
+            Assert.Equals(__list.Count, 6);
         }
     }
 }
